350a76c894c722c6dd3e13244839cdf69610a966
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-10-07 01:53:51 +0600

    feat strip payment method added

--- controllers/stripPaymentController.js
+++ controllers/stripPaymentController.js
@@ -1,5 +1,9 @@
 const Stripe = require("stripe");
+const client = require("../config/db");
+const { ObjectId } = require("mongodb");
 const stripe = Stripe(process.env.PAYMENT_SECRET_KEY);
+const paymentCollection = client.db("bistroNestDb").collection("payments");
+const cartCollection = client.db("bistroNestDb").collection("carts");
 
 const stripePaymentIntent = async (req, res) => {
   try {
@@ -18,4 +22,16 @@ const stripePaymentIntent = async (req, res) => {
   }
 };
 
-module.exports = { stripePaymentIntent };
+const paymentHistory = async (req, res) => {
+  const payment = req.body;
+  const paymentResult = await paymentCollection.insertOne(payment);
+  const query = {
+    _id: {
+      $in: payment.cartIds.map((id) => new ObjectId(id)),
+    },
+  };
+  const deleteResult = await cartCollection.deleteMany(query);
+  res.send({ paymentResult, deleteResult });
+};
+
+module.exports = { stripePaymentIntent, paymentHistory };
--- routes/stripPaymentRoutes.js
+++ routes/stripPaymentRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { stripePaymentIntent } = require("../controllers/stripPaymentController");
+const {
+  stripePaymentIntent,
+  paymentHistory,
+} = require("../controllers/stripPaymentController");
 
 const router = express.Router();
 
 router.post("/create-payment-intent", stripePaymentIntent);
+router.post("/payment", paymentHistory);
 
 module.exports = router;
