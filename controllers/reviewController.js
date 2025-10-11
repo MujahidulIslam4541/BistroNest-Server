ed609c20c5ab73ca6e7618f9233fa27b7257870d
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-10-12 00:24:10 +0600

    feat: add review submission endpoint and update routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,16 @@
 const client = require("../config/db");
 const reviewsCollection = client.db("bistroNestDb").collection("reviews");
 
+exports.review = async (req, res) => {
+  try {
+    const data = req.body;
+    const result = await reviewsCollection.insertOne(data);
+    res.send(result);
+  } catch (error) {
+    return res.status(500).send({message:"review not added"})
+  }
+};
+
 exports.getReview = async (req, res) => {
   const result = await reviewsCollection.find().toArray();
   res.send(result);
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const router=express.Router();
-const {getReview}=require('../controllers/reviewController')
+const {getReview, review}=require('../controllers/reviewController');
+const { verifyToken } = require('../controllers/jwtController');
 
+router.post('/reviews',verifyToken,review)
 router.get('/reviews',getReview)
 module.exports=router;
