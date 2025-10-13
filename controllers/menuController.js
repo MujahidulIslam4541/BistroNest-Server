c64b96e7ba7ff21f8a7d23e05128d6898b28ae2b
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-10-13 23:54:17 +0600

    feat: implement pagination for menu retrieval and update routes

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -7,6 +7,41 @@ exports.getMenu = async (req, res) => {
   res.send(result);
 };
 
+
+// get menu with pagination
+exports.getMenuWithPagination = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1; // default page = 1
+    const limit = parseInt(req.query.limit) || 0; // default 0 → all items
+    let items, total, totalPages;
+
+    if (limit > 0) {
+      const skip = (page - 1) * limit;
+      total = await menuCollection.countDocuments();
+      items = await menuCollection.find().skip(skip).limit(limit).toArray();
+      totalPages = Math.ceil(total / limit);
+    } else {
+      items = await menuCollection.find().toArray();
+      total = items.length;
+      totalPages = 1;
+    }
+
+    res.json({
+      success: true,
+      data: items,
+      page: limit ? page : 1,
+      perPage: limit || total,
+      total,
+      totalPages,
+    });
+
+    // console.log(success,items,page,perPage,total,totalPages)
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
 exports.menuPost = async (req, res) => {
   const menu = req.body;
   const result = await menuCollection.insertOne(menu);
@@ -23,7 +58,7 @@ exports.deleteMenu = async (req, res) => {
 // TODO:NOT working get item by id
 exports.getMenuById = async (req, res) => {
   const id = req.params.id;
-  const query = { _id: (id) };
+  const query = { _id: id };
   const result = await menuCollection.findOne(query);
   res.send(result);
 };
--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -5,12 +5,14 @@ const {
   menuPost,
   deleteMenu,
   getMenuById,
+  getMenuWithPagination,
 } = require("../controllers/menuController");
 const { verifyToken } = require("../controllers/jwtController");
 const { verifyAdmin } = require("../controllers/userController");
 
 router.get("/menu", getMenu);
 router.post("/menu", verifyToken, verifyAdmin, menuPost);
+router.get("/menuPagination", verifyToken, verifyAdmin, getMenuWithPagination);
 router.delete("/menu/:id", verifyToken, verifyAdmin, deleteMenu);
 router.get("/menu/:id", getMenuById);
 module.exports = router;
